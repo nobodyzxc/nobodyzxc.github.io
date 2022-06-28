f785b873fe85144e368166751e820ddf5a67815d
nobodyzxc <nobodyzxc.tw@gmail.com>
2022-06-29 07:20:22 +0800

    Site updated: 2022-06-29 07:20:22

--- js/src/custom.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $(window).scroll(function(){
-    $(".top-scroll-bar").attr("style", "width: " + ($(this).scrollTop() / ($(document).height() - $(this).height()) * 100) + "%; display: block;");
-  });
-});
