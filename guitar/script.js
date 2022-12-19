c8b1e7494ff5d97da7a9c5d8cc89049908e1024e
nobodyzxc <nobodyzxc.tw@gmail.com>
2022-12-20 00:48:02 +0800

    Site updated: 2022-12-20 00:48:02

--- guitar/script.js
+++ guitar/script.js
@@ -17,76 +17,6 @@ var notes = {
   b: ['B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
 }
 
-for (var i=0; i < notes.e.length; i++){
-  $('.mask.low-e ul')
-    .append('<li note='+notes.e[i]+'>'+notes.e[i]+'</li>')
-  $('.mask.a ul')
-    .append('<li note='+notes.a[i]+'>'+notes.a[i]+'</li>')
-  $('.mask.d ul')
-    .append('<li note='+notes.d[i]+'>'+notes.d[i]+'</li>')
-  $('.mask.g ul')
-    .append('<li note='+notes.g[i]+'>'+notes.g[i]+'</li>')
-  $('.mask.b ul')
-    .append('<li note='+notes.b[i]+'>'+notes.b[i]+'</li>')
-  $('.mask.high-e ul')
-    .append('<li note='+notes.e[i]+'>'+notes.e[i]+'</li>')
-  $('.mask').css({right: -189 + noteOffset})
-}
-
-$('.controls a.down').click(function(){
-  if(!canClick){return false;}
-  canClick = false;
-
-  $('.mask').each(function(){
-    var el = $(this);
-    var nextNote = el.find('li:nth-child(12)').text();
-
-    el.animate({right: -268 + noteOffset}, slideSpeed);
-    setTimeout(function(){
-      el.find('ul').prepend( "<li note="+nextNote+">"+nextNote+"</li>" );
-      el.find('li:last-child').remove();
-      el.css({right: -189 + noteOffset});
-    }, slideSpeed+20)
-  });
-
-  setTimeout(function(){
-    changeOpenNotes();
-    showNotes(noteToShow);
-    canClick = true;
-  }, slideSpeed+20)
-
-  return false;
-});
-
-$('.controls a.up').click(function(){
-  if(!canClick){return false;}
-  canClick = false;
-
-  $('.mask').each(function(){
-    var el = $(this);
-    var nextNote = el.find('li:nth-child(2)').text();
-
-    $( "<li note="+nextNote+">"+nextNote+"</li>" ).appendTo(el.find('ul'));
-    el.css({right: -268 + noteOffset});
-    el.find('li:first-child').remove();
-    el.animate({right: -189 + noteOffset}, slideSpeed);
-
-  });
-
-  changeOpenNotes();
-  showNotes(noteToShow);
-
-  setTimeout(function(){
-    canClick = true;
-  }, slideSpeed+20)
-  return false;
-});
-
-$('.controls li').click(function(){
-  noteToShow = $(this).text();
-  showNotes(noteToShow);
-});
-
 function showNotes(noteToShow = ''){
   noteToShow = noteToShow.trim();
   if(!noteToShow || noteToShow == 'No'){
@@ -193,6 +123,77 @@ function showNote(){
 }
 
 $(document).ready(function(){
+
+  for (var i=0; i < notes.e.length; i++){
+    $('.mask.low-e ul')
+      .append('<li note='+notes.e[i]+'>'+notes.e[i]+'</li>')
+    $('.mask.a ul')
+      .append('<li note='+notes.a[i]+'>'+notes.a[i]+'</li>')
+    $('.mask.d ul')
+      .append('<li note='+notes.d[i]+'>'+notes.d[i]+'</li>')
+    $('.mask.g ul')
+      .append('<li note='+notes.g[i]+'>'+notes.g[i]+'</li>')
+    $('.mask.b ul')
+      .append('<li note='+notes.b[i]+'>'+notes.b[i]+'</li>')
+    $('.mask.high-e ul')
+      .append('<li note='+notes.e[i]+'>'+notes.e[i]+'</li>')
+    $('.mask').css({right: -189 + noteOffset})
+  }
+
+  $('.controls a.down').click(function(){
+    if(!canClick){return false;}
+    canClick = false;
+
+    $('.mask').each(function(){
+      var el = $(this);
+      var nextNote = el.find('li:nth-child(12)').text();
+
+      el.animate({right: -268 + noteOffset}, slideSpeed);
+      setTimeout(function(){
+        el.find('ul').prepend( "<li note="+nextNote+">"+nextNote+"</li>" );
+        el.find('li:last-child').remove();
+        el.css({right: -189 + noteOffset});
+      }, slideSpeed+20)
+    });
+
+    setTimeout(function(){
+      changeOpenNotes();
+      showNotes(noteToShow);
+      canClick = true;
+    }, slideSpeed+20)
+
+    return false;
+  });
+
+  $('.controls a.up').click(function(){
+    if(!canClick){return false;}
+    canClick = false;
+
+    $('.mask').each(function(){
+      var el = $(this);
+      var nextNote = el.find('li:nth-child(2)').text();
+
+      $( "<li note="+nextNote+">"+nextNote+"</li>" ).appendTo(el.find('ul'));
+      el.css({right: -268 + noteOffset});
+      el.find('li:first-child').remove();
+      el.animate({right: -189 + noteOffset}, slideSpeed);
+
+    });
+
+    changeOpenNotes();
+    showNotes(noteToShow);
+
+    setTimeout(function(){
+      canClick = true;
+    }, slideSpeed+20)
+    return false;
+  });
+
+  $('.controls li').click(function(){
+    noteToShow = $(this).text();
+    showNotes(noteToShow);
+  });
+
   $('#mode').change(function(){
     resetMode();
     window[$(this).val()]?.();
@@ -210,4 +211,30 @@ $(document).ready(function(){
     window[$('#mode').val()]?.();
   });
   showNotes();
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 });
