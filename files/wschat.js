f1939f653f19dbf75c7f952a5566f9fe85cce2b5
nobodyzxc <nobodyzxc.tw@gmail.com>
2025-12-24 02:50:01 +0800

    Site updated: 2025-12-24 02:49:59

--- files/wschat.js
+++ files/wschat.js
@@ -9,19 +9,19 @@ if (window.location.protocol == "https:") {
 //var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
 var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/wschat");
 
-//inbox.onmessage = function(message) {
-//  var data = JSON.parse(message.data);
-//  $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
-//  $("#chat-text").stop().animate({
-//    scrollTop: $('#chat-text')[0].scrollHeight
-//  }, 800);
-//};
-//
-//inbox.onclose = function(){
-//    console.log('inbox closed');
-//    this.inbox = new WebSocket(inbox.url);
-//
-//};
+inbox.onmessage = function(message) {
+  var data = JSON.parse(message.data);
+  $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
+  $("#chat-text").stop().animate({
+    scrollTop: $('#chat-text')[0].scrollHeight
+  }, 800);
+};
+
+inbox.onclose = function(){
+    console.log('inbox closed');
+    this.inbox = new WebSocket(inbox.url);
+
+};
 
 outbox.onclose = function(){
     console.log('outbox closed');
